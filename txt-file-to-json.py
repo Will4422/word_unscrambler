777050809f8332f18c66ceabaf4d7cdb8e34a4ee
UBC Student <will@oxtoby.ca>
2023-05-09 12:43:31 -0700

    created JSON file of all words, sorted on length

--- txt-file-to-json.py
+++ txt-file-to-json.py
@@ -1,11 +1,21 @@
-def load_words():
-    with open('words_alpha.txt') as word_file:
-        valid_words = set(word_file.read().split())
+import json;
 
-    return valid_words
+def to_json():
+    dict = {}
+    for i in range(1, 100):
+        dict[i] = []
+   
+
+    with open('words_alpha.txt') as word_file:
+        for line in word_file:
+            line.strip()
+            str_len = line.__len__() - 1 # -1 to remove \n
+            dict[str_len].append({line.strip() : 0})
+
+    json_file = open('words_alpha.json', 'w')
+    json.dump(dict, json_file, indent=4)
+    json_file.close()
 
 
 if __name__ == '__main__':
-    english_words = load_words()
-    # demo print
-    print(type(english_words))
+    to_json()
