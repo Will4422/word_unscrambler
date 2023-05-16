84f5a5f0d2e98c593f23ebb219424ac191817d87
UBC Student <will@oxtoby.ca>
2023-05-16 00:28:54 -0700

    added test file

--- app/src/executeGetRequest.js
+++ app/src/executeGetRequest.js
@@ -2,6 +2,6 @@ function executeGetRequest() {
     fetch('https://will4422.github.io/word_unscrambler/words_alpha.json')
         .then(resp => resp.json())
         .then(json => console.log(JSON.stringify(json)))
-    }
+}
 
 export default executeGetRequest;
