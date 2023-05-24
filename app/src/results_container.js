a270dfa7b908d4ed33fd09391e3453bcafcd4ecf
UBC Student <will@oxtoby.ca>
2023-05-23 21:58:13 -0700

    basic functionality complete

--- app/src/results_container.js
+++ app/src/results_container.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+/* 
+    How do I use the this.props.results object?
+        this.props.results is the entire json object, it just contains keys 1 - 99 and each key has a corresponding value
+            which is an array
+        this.props.results[1] should represent the length of the string that we want to investigate
+            so this is the object that we want to iterate through
+*/
+
 class ResultsContainer extends React.Component {
     constructor(props) {
         super(props);
@@ -9,7 +17,10 @@ class ResultsContainer extends React.Component {
         return (
             <div>
                 <p>Results are displayed!</p>
-                <p>The results = {this.props.results["title"]}</p>
+                {/* <p>Object.keys(Object.keys(this.props.results)) = {Object.keys(this.props.results)}</p>
+                <p>Object.keys(Object.keys(this.props.result[1])) = {Object.keys(this.props.results[1])}</p>
+                <p>Object.keys(Object.keys(this.props.result[2])) = {Object.keys(this.props.results[2])}</p> */}
+                <p>The results = {this.props.scrambledStringProp}</p>
             </div>
         )
     }
--- app/src/unscrambleForm.js
+++ app/src/unscrambleForm.js
@@ -11,16 +11,55 @@ class unscrambleForm extends React.Component {
         this.state = {
             displayResults: false,
             results: {},
+            scrambledString: "",
+            scrambledStringLength: 0,
+            possibleSolutions: []
         }
     }
     
     async unscramble(event) {
         event.preventDefault();
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        // fetch the json file data
+        const res = await fetch('https://will4422.github.io/word_unscrambler/words_alpha.json');
         const data = await res.json();
+
+        // Read the form data
+        const form = event.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        const scrambledString = formJson["inputString"];
+
+        // find appropriate potentially matching strings
+        const scrambledStringList = scrambledString.split('');
+        const length = scrambledString.length;
+        const allPossibleWordsByLength = Object.values(Object.values(data[length]));
+        const possibleSolutions = [];
+
+        // search through possible words by length of words
+        allPossibleWordsByLength.forEach(word => {
+            let shouldInclude = true;
+            let wordUnderInvestigation = Object.keys(word)[0];
+            let wordUnderInvestigationList = wordUnderInvestigation.split('');
+            let sorted1 = scrambledStringList.sort();
+            let sorted2 = wordUnderInvestigationList.sort();
+            for (let i = 0; i < length; i++) {
+                if (sorted1[i] !== sorted2[i]) {
+                    shouldInclude = false;
+                }
+            }
+            if (shouldInclude === true) {
+                possibleSolutions.push(wordUnderInvestigation);
+            }
+        });
+        debugger;
+
+
         this.setState({
             displayResults: true,
             results: data,
+            scrambledString: scrambledString,
+            scrambledStringLength: length,
+            possibleSolutions: possibleSolutions
         });
     }
     
@@ -33,12 +72,14 @@ class unscrambleForm extends React.Component {
                     <Row>
                         <div className="well">
                             <form onSubmit={this.unscramble}>
-                            <input></input>
+                                <label>
+                                    Your letters: <input name="inputString" autoFocus={true}/>   
+                                </label>
                             <button type='submit'>Unscramble!</button>
                             </form>
                         </div>
                     </Row>
-                    {this.state.displayResults && <ResultsContainer results={this.state.results} />}
+                    {this.state.displayResults && this.state.possibleSolutions.map(solution => (<p>{solution}</p>))}
                 </div>
             </div>
         );
